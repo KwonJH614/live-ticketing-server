50dd21595c7fee2bf50f6b2f6c25cc38086d9bc8
madogkwon <a93081077a@gmail.com>
2025-11-10 09:59:55 +0900

    fix: User 엔티티 createdAt 필드에 @Column("created_at") 추가

--- src/main/java/org/example/ticketing/domain/user/entity/User.java
+++ src/main/java/org/example/ticketing/domain/user/entity/User.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.example.ticketing.domain.user.enums.Role;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -25,5 +26,6 @@ public class User {
   private Role role;
 
   @CreatedDate
+  @Column("created_at")
   private LocalDateTime createdAt;
 }
