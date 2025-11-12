601e6b030ca74c63009597958dced6339975c648
madogkwon <a93081077a@gmail.com>
2025-11-12 11:09:39 +0900

    fix: jwt 식별자를 email에서 username으로 변경

--- src/main/java/org/example/ticketing/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/ticketing/domain/user/repository/UserRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<User, Long> {
-  Mono<User> findByEmail(String email);
+  Mono<User> findByUsername(String username);
   Mono<Boolean> existsByUsername(String username);
 }
