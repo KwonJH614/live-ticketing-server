423793074f4a71c1c09e5cc2ab5b5a2941adac7f
madogkwon <a93081077a@gmail.com>
2025-11-12 15:01:36 +0900

    feat: 이벤트 생성기능 구현

---
+++ src/main/java/org/example/ticketing/domain/event/service/EventService.java
@@ -0,0 +1,56 @@
+package org.example.ticketing.domain.event.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.ticketing.common.exception.PastEventDateException;
+import org.example.ticketing.common.exception.UserNotFoundException;
+import org.example.ticketing.domain.event.dto.CreateEventRequestDto;
+import org.example.ticketing.domain.event.dto.CreateEventResponseDto;
+import org.example.ticketing.domain.event.entity.Event;
+import org.example.ticketing.domain.event.repository.EventRepository;
+import org.example.ticketing.domain.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class EventService {
+  private final EventRepository eventRepository;
+  private final UserRepository userRepository;
+
+  public Mono<CreateEventResponseDto> createEvent(CreateEventRequestDto dto, String username) {
+    return Mono.just(dto)
+            .flatMap(request -> {
+              if (request.getStartTime().isBefore(LocalDateTime.now())) {
+                return Mono.error(new PastEventDateException());
+              }
+              return Mono.just(request);
+            })
+            .flatMap(request ->
+              userRepository.existsByUsername(username)
+                      .flatMap(exists -> {
+                        if (!exists) {
+                          return Mono.error(new UserNotFoundException(username));
+                        }
+                        return Mono.just(request);
+                      })
+            )
+            .flatMap(request -> {
+              Event event = Event.builder()
+                      .title(request.getTitle())
+                      .description(request.getDescription())
+                      .startTime(request.getStartTime())
+                      .endTime(request.getEndTime())
+                      .venue(request.getVenue())
+                      .price(request.getPrice())
+                      .build();
+
+              return eventRepository.save(event);
+            })
+            .map(savedEvent -> new CreateEventResponseDto(
+                    savedEvent.getId(),
+                    savedEvent.getTitle()
+            ));
+  }
+}
