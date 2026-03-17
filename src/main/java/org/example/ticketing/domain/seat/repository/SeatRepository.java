e523b8eaade6d56f210415477d04210e55972cb0
KwonJH614 <madogkwon@gmail.com>
2026-03-17 17:31:57 +0900

    fix: @Modifying 추가, 테이블명/컬럼명 일치

--- src/main/java/org/example/ticketing/domain/seat/repository/SeatRepository.java
+++ src/main/java/org/example/ticketing/domain/seat/repository/SeatRepository.java
@@ -1,6 +1,7 @@
 package org.example.ticketing.domain.seat.repository;
 
 import org.example.ticketing.domain.seat.entity.Seat;
+import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
@@ -10,8 +11,10 @@ import java.util.List;
 
 public interface SeatRepository extends ReactiveCrudRepository<Seat, Long> {
   Flux<Seat> findAllByEventIdOrderById(Long eventId);
-  @Query("UPDATE seat SET reserved = true WHERE id = :seatId AND reserved = false")
+  @Modifying
+  @Query("UPDATE seats SET is_reserved = true WHERE id = :seatId AND is_reserved = false")
   Mono<Integer> reserveIfAvailable(Long seatId);
-  @Query("UPDATE seat SET reserved = false WHERE id = :seatId")
+  @Modifying
+  @Query("UPDATE seats SET is_reserved = false WHERE id = :seatId")
   Mono<Integer> releaseSeat(Long seatId);
 }
